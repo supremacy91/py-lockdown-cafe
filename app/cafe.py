a12fe7301f37939bd590ea4b39eadbae79f6ca55
Maks Kaplunovskiy <kaplunovskymv@gmail.com>
2026-05-27 21:23:58 +0300

    implementation py-lockdown-cafe fix2

--- app/cafe.py
+++ app/cafe.py
@@ -11,7 +11,7 @@ class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> str:
+    def visit_cafe(self, visitor: dict[str, object]) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
 
